7203da42d96595f1381061463ab938dd6e493681
ZeroInfinity12th <atilla@12thwonder.com>
2017-08-06 17:30:54 -0700

    Adding No duplicate sorting JS file

--- README.md
+++ README.md
@@ -7,4 +7,3 @@ This is a sorting algorithm without duplicate numbers.
  I have also written a code using the same system to just sort it with duplicate numbers. It is not as fast as removing duplicate numbers.
  I am working on it to make it as fast as removing duplicate numbers or get it to as fast as any other sorting functions out that is being used already.
 
- 
---
+++ atillaNoDuplicateSorting.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ZeroInfinity on 8/6/2017.
+ */
+function AtillaNoDuplicateSorting(numbers) {
+    let collection = [];
+    for (let ni = 0; ni < numbers.length; ni++) {
+        let tmp = numbers[ni];
+        //console.log("Tmp number in distinct ----",tmp);
+        //console.log("If condition in distinct --", !nNumbers[tmp]);
+        if (!collection[tmp]) {
+            //let arr = [];
+            //arr.push(tmp);
+            collection[tmp] = [tmp];
+        }
+        else {
+            collection[tmp].push(tmp);
+        }
+    }
+    //return collection;
+    let sortedArr = [];
+    for(let i = 0; i <collection.length; i++) {
+        if(collection[i]) {
+            let inArray = collection[i];
+            for(let ii = 0; ii<inArray.length; ii++) {
+                sortedArr.push(inArray[ii]);
+            }
+        }
+    }
+
+    return sortedArr;
+}
