891790eeeaab9bee326eae37381d53424cf66e47
ayfortworthtx <ayfortworthtx@gmail.com>
2024-01-07 11:42:40 -0600

    Small set of number test with quicksort

--- atillaNoDuplicateSorting.js
+++ atillaNoDuplicateSorting.js
@@ -10,7 +10,7 @@ function AtillaNoDuplicateSorting(numbers) {
     if (!collection[tmp]) {
       //let arr = [];
       //arr.push(tmp);
-      collection[tmp] = [tmp];
+      collection[tmp] = tmp;
     }
   }
   //return collection;
@@ -24,51 +24,92 @@ function AtillaNoDuplicateSorting(numbers) {
   return sortedArr;
 }
 
-function AtillaNoDuplicateSortingMergeSort(numbers) {
-  let collection = [];
-  for (let ni = 0; ni < numbers.length; ni++) {
-    let tmp = numbers[ni];
-    //console.log("Tmp number in distinct ----",tmp);
-    //console.log("If condition in distinct --", !nNumbers[tmp]);
-    if (!collection[tmp]) {
-      //let arr = [];
-      //arr.push(tmp);
-      collection[tmp] = [tmp];
-    }
-  }
-  //return collection;
-  let sortedArr = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (collection[i]) {
-      sortedArr.push(collection[i]);
-    }
-  }
+// function AtillaNoDuplicateSortingMergeSort(numbers) {
+//   let collection = [];
+//   for (let ni = 0; ni < numbers.length; ni++) {
+//     let tmp = numbers[ni];
+//     //console.log("Tmp number in distinct ----",tmp);
+//     //console.log("If condition in distinct --", !nNumbers[tmp]);
+//     if (!collection[tmp]) {
+//       //let arr = [];
+//       //arr.push(tmp);
+//       collection[tmp] = [tmp];
+//     }
+//   }
+//   //return collection;
+//   let sortedArr = [];
+//   for (let i = 0; i < collection.length; i++) {
+//     if (collection[i]) {
+//       sortedArr.push(collection[i]);
+//     }
+//   }
 
-  return sortedArr;
+//   return sortedArr;
+// }
+
+// function mergeSort(arr) {
+//   // Base case
+//   if (arr.length <= 1) return arr;
+//   let mid = Math.floor(arr.length / 2);
+//   // Recursive calls
+//   let left = mergeSort(arr.slice(0, mid));
+//   let right = mergeSort(arr.slice(mid));
+//   return merge(left, right);
+// }
+
+// function merge(left, right) {
+//   let sortedArr = []; // the sorted items will go here
+//   while (left.length && right.length) {
+//     // Insert the smallest item into sortedArr
+//     if (left[0] < right[0]) {
+//       sortedArr[left] = left;
+//     } else {
+//       sortedArr[righ] = right;
+//     }
+//   }
+//   // Use spread operators to create a new array, combining the three arrays
+//   return [...sortedArr, ...left, ...right];
+// }
+
+function swap(items, leftIndex, rightIndex) {
+  var temp = items[leftIndex];
+  items[leftIndex] = items[rightIndex];
+  items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+  var pivot = items[Math.floor((right + left) / 2)], //middle element
+    i = left, //left pointer
+    j = right; //right pointer
+  while (i <= j) {
+    while (items[i] < pivot) {
+      i++;
+    }
+    while (items[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(items, i, j); //sawpping two elements
+      i++;
+      j--;
+    }
+  }
+  return i;
 }
 
-function mergeSort(arr) {
-  // Base case
-  if (arr.length <= 1) return arr;
-  let mid = Math.floor(arr.length / 2);
-  // Recursive calls
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-}
-
-function merge(left, right) {
-  let sortedArr = []; // the sorted items will go here
-  while (left.length && right.length) {
-    // Insert the smallest item into sortedArr
-    if (left[0] < right[0]) {
-      sortedArr[left] = left;
-    } else {
-      sortedArr[righ] = right;
+function quickSort(items, left, right) {
+  var index;
+  if (items.length > 1) {
+    index = partition(items, left, right); //index returned from partition
+    if (left < index - 1) {
+      //more elements on the left side of the pivot
+      quickSort(items, left, index - 1);
+    }
+    if (index < right) {
+      //more elements on the right side of the pivot
+      quickSort(items, index, right);
     }
   }
-  // Use spread operators to create a new array, combining the three arrays
-  return [...sortedArr, ...left, ...right];
+  return items;
 }
 
 function generateRandomNumbersByquantityAndMax(quantity, max, min) {
@@ -81,9 +122,19 @@ function generateRandomNumbersByquantityAndMax(quantity, max, min) {
   return arr;
 }
 
-let numbers = generateRandomNumbersByquantityAndMax(1000000, 100000000, 1000);
+let numbers = generateRandomNumbersByquantityAndMax(1000000, 100000, 10);
 
-console.time('start');
+console.time('quick sorting');
+let quickSortsorted = quickSort(numbers, 0, numbers.length - 1);
+console.timeLog('quick sorting');
+console.timeEnd('quick sorting');
+
+// console.log(numbers);
+console.log(numbers.length);
+console.time('sorting');
 let sorted = AtillaNoDuplicateSorting(numbers);
-console.timeEnd('start');
-console.timeLog('start');
+console.timeLog('sorting');
+console.timeEnd('sorting');
+console.log('sorted', sorted.length);
+
+// console.log(sorted);
