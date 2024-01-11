8fcc4954211cf4eb5a0b01da56dbe54152cc79b1
ayfortworthtx <ayfortworthtx@gmail.com>
2024-01-10 18:47:17 -0600

    Not including duplicates with simple logic

--- atillaNoDuplicateSorting.js
+++ atillaNoDuplicateSorting.js
@@ -3,80 +3,84 @@
  */
 function AtillaNoDuplicateSorting(numbers) {
   let collection = [];
-  let minNumber = 100000;
+  let minNumber;
   for (let ni = 0; ni < numbers.length; ni++) {
     let tmp = numbers[ni];
     //console.log(`tmp is ${tmp}`);
-    if (tmp > 0 && minNumber > tmp) {
+    if (tmp > 0 && !minNumber) {
       minNumber = tmp;
-      console.log(`min number is ${minNumber}`);
+    } else if (tmp > 0 && minNumber > tmp) {
+      minNumber = tmp;
+      //console.log(`min number is ${minNumber}`);
     }
     //console.log("Tmp number in distinct ----",tmp);
     //console.log("If condition in distinct --", !nNumbers[tmp]);
-    if (!collection[tmp]) {
-      //let arr = [];
-      //arr.push(tmp);
-      collection[tmp] = tmp;
-    }
+    //let arr = [];
+    //arr.push(tmp);
+    collection[tmp] = tmp;
   }
   //return collection;
   let sortedArr = [];
+  //  let sortedArrSet = new Set(collection);
   for (let i = minNumber; i < collection.length; i++) {
     if (collection[i]) {
       sortedArr.push(collection[i]);
     }
   }
-  console.log(`minNumber is ${minNumber}`);
+  // console.log(`minNumber is ${minNumber}`);
   return sortedArr;
 }
+function MergeSort(numbers) {
+  let collection = [];
 
-// function AtillaNoDuplicateSortingMergeSort(numbers) {
-//   let collection = [];
-//   for (let ni = 0; ni < numbers.length; ni++) {
-//     let tmp = numbers[ni];
-//     //console.log("Tmp number in distinct ----",tmp);
-//     //console.log("If condition in distinct --", !nNumbers[tmp]);
-//     if (!collection[tmp]) {
-//       //let arr = [];
-//       //arr.push(tmp);
-//       collection[tmp] = [tmp];
-//     }
-//   }
-//   //return collection;
-//   let sortedArr = [];
-//   for (let i = 0; i < collection.length; i++) {
-//     if (collection[i]) {
-//       sortedArr.push(collection[i]);
-//     }
-//   }
+  return AtillaNoDuplicateSortingMergeSort(numbers);
 
-//   return sortedArr;
-// }
+  function AtillaNoDuplicateSortingMergeSort(numbers) {
+    for (let ni = 0; ni < numbers.length; ni++) {
+      let tmp = numbers[ni];
+      //console.log("Tmp number in distinct ----",tmp);
+      //console.log("If condition in distinct --", !nNumbers[tmp]);
+      if (!collection[tmp]) {
+        //let arr = [];
+        //arr.push(tmp);
+        collection[tmp] = [tmp];
+      }
+    }
+    //return collection;
+    let sortedArr = [];
+    for (let i = 0; i < collection.length; i++) {
+      if (collection[i]) {
+        sortedArr.push(collection[i]);
+      }
+    }
 
-// function mergeSort(arr) {
-//   // Base case
-//   if (arr.length <= 1) return arr;
-//   let mid = Math.floor(arr.length / 2);
-//   // Recursive calls
-//   let left = mergeSort(arr.slice(0, mid));
-//   let right = mergeSort(arr.slice(mid));
-//   return merge(left, right);
-// }
+    return sortedArr;
+  }
 
-// function merge(left, right) {
-//   let sortedArr = []; // the sorted items will go here
-//   while (left.length && right.length) {
-//     // Insert the smallest item into sortedArr
-//     if (left[0] < right[0]) {
-//       sortedArr[left] = left;
-//     } else {
-//       sortedArr[righ] = right;
-//     }
-//   }
-//   // Use spread operators to create a new array, combining the three arrays
-//   return [...sortedArr, ...left, ...right];
-// }
+  function mergeSort(arr) {
+    // Base case
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    // Recursive calls
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+  }
 
+  function merge(left, right) {
+    let sortedArr = []; // the sorted items will go here
+    while (left.length && right.length) {
+      // Insert the smallest item into sortedArr
+      if (left[0] < right[0]) {
+        sortedArr[left] = left;
+      } else {
+        sortedArr[righ] = right;
+      }
+    }
+    // Use spread operators to create a new array, combining the three arrays
+    return [...sortedArr, ...left, ...right];
+  }
+}
 function swap(items, leftIndex, rightIndex) {
   var temp = items[leftIndex];
   items[leftIndex] = items[rightIndex];
@@ -101,7 +105,6 @@ function partition(items, left, right) {
   }
   return i;
 }
-
 function quickSort(items, left, right) {
   var index;
   if (items.length > 1) {
@@ -117,7 +120,6 @@ function quickSort(items, left, right) {
   }
   return items;
 }
-
 function generateRandomNumbersByquantityAndMax(quantity, max, min) {
   const arr = [];
   while (arr.length < quantity) {
@@ -128,21 +130,33 @@ function generateRandomNumbersByquantityAndMax(quantity, max, min) {
   return arr;
 }
 
-let numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 10000);
+let numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 90000);
 console.dir(`numbers is ${numbers}`);
-console.time('quick sorting');
-let quickSortsorted = quickSort(numbers, 0, numbers.length - 1);
-console.timeLog('quick sorting');
-console.timeEnd('quick sorting');
 
-console.log('sorted', quickSortsorted.length);
+// console.log(numbers.length);
 
-// console.log(numbers);
-console.log(numbers.length);
+// console.time('quick sorting');
+// let quickSortsorted = quickSort(numbers, 0, numbers.length - 1);
+// console.timeLog('quick sorting');
+// console.timeEnd('quick sorting');
+
+// console.log('sorted', quickSortsorted.length);
+
+// console.dir(`numbers is ${numbers}`);
+// numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 10000);
 console.time('sorting');
 let sorted = AtillaNoDuplicateSorting(numbers);
 console.timeLog('sorting');
 console.timeEnd('sorting');
 console.log('sorted', sorted.length);
+
+// console.time('merge sorting');
+// let mergeSortsorted = MergeSort(numbers);
+// console.timeLog('merge sorting');
+// console.timeEnd('merge sorting');
+
+// console.log('merge sorted', mergeSortsorted.length);
+
+// console.log(numbers);
 
 // console.log(sorted);
