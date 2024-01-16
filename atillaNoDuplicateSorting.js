d67cdf951d1d2323303303bb6e14dce4268d64ce
ayfortworthtx <ayfortworthtx@gmail.com>
2024-01-15 20:39:42 -0600

    Just leaving my code and random number generator

--- atillaNoDuplicateSorting.js
+++ atillaNoDuplicateSorting.js
@@ -5,15 +5,9 @@
 function AtillaNoDuplicateSorting(numbers) {
   let collection = [];
   let minNumber;
-  let middle = Math.floor((numbers.length - 1) / 2);
-  let quarter = Math.floor((middle - 1) / 2);
+  let middle = Math.floor((numbers.length - 1) / 2) + 1;
+  let quarter = Math.floor((middle - 1) / 2) + 1;
   let third = middle + quarter;
-
-  console.log(`middle numbers is ${middle}`);
-  console.log(`middle numbers is ${quarter}`);
-  console.log(`middle numbers is ${third}`);
-  // first iter is
-  console.log('First Loop');
 
   for (let ni = 0; ni < quarter; ni++) {
     let tmp = numbers[ni];
@@ -38,6 +32,7 @@ function AtillaNoDuplicateSorting(numbers) {
     }
     collection[tmp] = tmp;
   }
+
   for (let ni = middle; ni < numbers.length; ni++) {
     let tmp = numbers[ni];
     //console.log(`tmp is ${tmp}`);
@@ -58,104 +53,17 @@ function AtillaNoDuplicateSorting(numbers) {
   return collection;
   let sortedArr = [];
   //  let sortedArrSet = new Set(collection);
-  for (let i = minNumber; i < collection.length; i++) {
+  //for (let i = minNumber; i < collection.length; i++) {
+
+  collection.forEach((i) => {
     if (collection[i]) {
       sortedArr.push(collection[i]);
     }
-  }
+  });
   // console.log(`minNumber is ${minNumber}`);
   return sortedArr;
 }
-function MergeSort(numbers) {
-  let collection = [];
 
-  return AtillaNoDuplicateSortingMergeSort(numbers);
-
-  function AtillaNoDuplicateSortingMergeSort(numbers) {
-    for (let ni = 0; ni < numbers.length; ni++) {
-      let tmp = numbers[ni];
-      //console.log("Tmp number in distinct ----",tmp);
-      //console.log("If condition in distinct --", !nNumbers[tmp]);
-      if (!collection[tmp]) {
-        //let arr = [];
-        //arr.push(tmp);
-        collection[tmp] = [tmp];
-      }
-    }
-    //return collection;
-    let sortedArr = [];
-    for (let i = 0; i < collection.length; i++) {
-      if (collection[i]) {
-        sortedArr.push(collection[i]);
-      }
-    }
-
-    return sortedArr;
-  }
-
-  function mergeSort(arr) {
-    // Base case
-    if (arr.length <= 1) return arr;
-    let mid = Math.floor(arr.length / 2);
-    // Recursive calls
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right);
-  }
-
-  function merge(left, right) {
-    let sortedArr = []; // the sorted items will go here
-    while (left.length && right.length) {
-      // Insert the smallest item into sortedArr
-      if (left[0] < right[0]) {
-        sortedArr[left] = left;
-      } else {
-        sortedArr[righ] = right;
-      }
-    }
-    // Use spread operators to create a new array, combining the three arrays
-    return [...sortedArr, ...left, ...right];
-  }
-}
-function swap(items, leftIndex, rightIndex) {
-  var temp = items[leftIndex];
-  items[leftIndex] = items[rightIndex];
-  items[rightIndex] = temp;
-}
-function partition(items, left, right) {
-  var pivot = items[Math.floor((right + left) / 2)], //middle element
-    i = left, //left pointer
-    j = right; //right pointer
-  while (i <= j) {
-    while (items[i] < pivot) {
-      i++;
-    }
-    while (items[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      swap(items, i, j); //sawpping two elements
-      i++;
-      j--;
-    }
-  }
-  return i;
-}
-function quickSort(items, left, right) {
-  var index;
-  if (items.length > 1) {
-    index = partition(items, left, right); //index returned from partition
-    if (left < index - 1) {
-      //more elements on the left side of the pivot
-      quickSort(items, left, index - 1);
-    }
-    if (index < right) {
-      //more elements on the right side of the pivot
-      quickSort(items, index, right);
-    }
-  }
-  return items;
-}
 function generateRandomNumbersByquantityAndMax(quantity, max, min) {
   const arr = [];
   while (arr.length < quantity) {
@@ -168,32 +76,21 @@ function generateRandomNumbersByquantityAndMax(quantity, max, min) {
 
 let numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 90000);
 let numbers2 = [...numbers];
-console.dir(`numbers is ${numbers}`);
+// console.dir(`numbers is ${numbers}`);
 
-console.dir(`numbers2 is ${numbers2}`);
-console.log(numbers.length);
+// console.dir(`numbers2 is ${numbers2}`);
+// console.log(numbers.length);
 
-console.time('quick sorting');
-let quickSortsorted = quickSort(numbers, 0, numbers.length - 1);
-console.timeLog('quick sorting');
-console.timeEnd('quick sorting');
-console.log('quick sorted', quickSortsorted.length);
+// console.time('quick sorting');
+// let quickSortsorted = quickSort(numbers, 0, numbers.length - 1);
+// console.timeLog('quick sorting');
+// console.timeEnd('quick sorting');
+// console.log('quick sorted', quickSortsorted.length);
 
 // console.dir(`numbers is ${numbers}`);
-numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 10000);
+//  numbers = generateRandomNumbersByquantityAndMax(1000000, 1000000, 10000);
 console.time('sorting');
 let sorted = AtillaNoDuplicateSorting(numbers2);
 console.timeLog('sorting');
 console.timeEnd('sorting');
 console.log('sorted', sorted.length);
-
-// console.time('merge sorting');
-// let mergeSortsorted = MergeSort(numbers);
-// console.timeLog('merge sorting');
-// console.timeEnd('merge sorting');
-
-// console.log('merge sorted', mergeSortsorted.length);
-
-// console.log(numbers);
-
-// console.log(sorted);
